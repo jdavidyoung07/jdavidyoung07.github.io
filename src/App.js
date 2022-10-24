32a691246d74b6b5204163d6ac57424892c1c348
Jean-David Young <jyoun111@uottawa.ca>
2022-10-24 15:59:54 -0400

    react modif

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div className="App">
       <h1>Hello React!</h1>
+      <h3>For local development, run "npm start".</h3>
+      <h3>To deploy changes to the github.io web page, run "npm run deploy".</h3>
     </div>
   );
 }
