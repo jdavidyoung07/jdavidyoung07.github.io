9ad9966292d19dbfbfc34299f196e3024c633479
Jean-David Young <jyoun111@uottawa.ca>
2022-10-24 16:21:56 -0400

    react setup part 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
     <div className="App">
       <h1>Hello React!</h1>
       <h3>For local development, run "npm start".</h3>
-      <h3>To deploy changes to the github.io web page, push changes to repo. If this is the first time, run "npm run deploy".</h3>
+      <h3>To deploy changes to the github.io web page, run "npm run deploy". Later, push changes to repo.</h3>
     </div>
   );
 }
